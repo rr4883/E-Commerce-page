35e40201019ea94deb5da3db7a3441a7cb540587
rr4883 <103633691+rr4883@users.noreply.github.com>
2024-01-08 18:47:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function change(){
+    let a=document.getElementById("img1");
+    a.style.display='block';
+    let b=document.getElementById("img2");
+    b.style.display='none';
+    let c=document.getElementById("img3");
+    c.style.display='none';
+}
+function change2(){
+    let a=document.getElementById("img1");
+    a.style.display='none';
+    let b=document.getElementById("img2");
+    b.style.display='block';
+    let c=document.getElementById("img3");
+    c.style.display='none';
+}
+function change1(){
+    let a=document.getElementById("img1");
+    a.style.display='none';
+    let b=document.getElementById("img2");
+    b.style.display='none';
+    let c=document.getElementById("img3");
+    c.style.display='block';
+
+}
